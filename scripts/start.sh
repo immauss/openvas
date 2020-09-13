71bccdbc83d482b973f7d9c38a248d08d82157bb
root <root@ames.immauss.com>
2020-09-13 06:06:15 +0000

    fixed NVT feed URL in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -100,7 +100,7 @@ if [ ! -f "/data/firstrun" ]; then
 fi
 
 echo "Updating NVTs..."
-su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.openvas.org:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
+su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
 sleep 5
 
 echo "Updating CERT data..."
