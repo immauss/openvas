aba44fc113cbf95cebcdd57ca5c6a61db5e701f7
GE Scott Knauss <scott@OldMac.local>
2020-12-31 16:29:11 +0100

    LOTS of tweaks and movmement to get 20.08 up and running...

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 ENV LANG=C.UTF-8
@@ -7,25 +7,29 @@ COPY install-pkgs.sh /install-pkgs.sh
 
 RUN bash /install-pkgs.sh
 
-ENV gvm_libs_version="v11.0.1" \
-    openvas_scanner_version="v7.0.1" \
-    gvmd_version="v9.0.1" \
-    gsa_version="v9.0.1" \
+ENV gvm_libs_version="v20.8.0" \
+    openvas_scanner_version="v20.8.0" \
+    gvmd_version="v20.8.0" \
+    gsa_version="v20.8.0" \
+    open_scanner_protocol_daemon="v20.8.1" \
+    ospd_openvas="v20.8.0" \
     gvm_tools_version="v2.1.0" \
     openvas_smb="v1.0.5" \
-    open_scanner_protocol_daemon="v2.0.1" \
-    ospd_openvas="v1.0.1" \
-    python_gvm_version="v1.5.0"
+    python_gvm_version="v1.6.0"
 
-RUN echo "Starting Build..." && mkdir /build
+
 
     #
     # install libraries module for the Greenbone Vulnerability Management Solution
     #
     
-RUN cd /build && \
+RUN echo "Starting Build..." && rm -rf /build &&\
+    mkdir -p /build && \
+    cd /build && \
+    pwd && \
     wget --no-verbose https://github.com/greenbone/gvm-libs/archive/$gvm_libs_version.tar.gz && \
     tar -zxf $gvm_libs_version.tar.gz && \
+    ls -l /build/ && \
     cd /build/*/ && \
     mkdir build && \
     cd build && \
@@ -102,7 +106,7 @@ RUN cd /build && \
     #
     # Install Greenbone Vulnerability Management Python Library
     #
-RUN python3 -m pip install python-gvm
+RUN python3 -m pip install python-gvm==$python_gvm_version
     
 #RUN cd /build && \
     #wget --no-verbose https://github.com/greenbone/python-gvm/archive/$python_gvm_version.tar.gz && \
@@ -139,14 +143,8 @@ RUN cd /build && \
     #
     # Install GVM-Tools
     #
-RUN python3 -m pip install gvm-tools    
-#RUN cd /build && \
-    #wget --no-verbose https://github.com/greenbone/gvm-tools/archive/$gvm_tools_version.tar.gz && \
-    #tar -zxf $gvm_tools_version.tar.gz && \
-    #cd /build/*/ && \
-    #python3 setup.py install && \
-    #echo "/usr/local/lib" > /etc/ld.so.conf.d/openvas.conf && ldconfig && cd / && rm -rf /build
-#
+RUN python3 -m pip install gvm-tools==$gvm_tools_version && \
+    echo "/usr/local/lib" > /etc/ld.so.conf.d/openvas.conf && ldconfig  
 
     # 
     # Make sure all libraries are linked and add a random directory suddenly needed by ospd :/
--- scripts/start.sh
+++ scripts/start.sh
@@ -5,17 +5,10 @@ USERNAME=${USERNAME:-admin}
 PASSWORD=${PASSWORD:-admin}
 RELAYHOST=${RELAYHOST:-172.17.0.1}
 SMTPPORT=${SMTPPORT:-25}
+REDISDBS=${REDISDBS:-512}
+QUIET=${QUIET:-false}
 
-echo "Setup / Fix the /usr/local/var/run"
-#if [ -d /usr/local/var/run ]; then
-	#echo "linking to '/run'"
-	#rm -rf /usr/local/var/run
-	#ln -s /run /usr/local/var/run
-	#chmod 777 /run
-#else
-	#echo "already linked to '/run'"
-#fi
-chmod 777 /usr/local/var/run
+
 
 if [ ! -d "/run/redis" ]; then
 	mkdir /run/redis
@@ -23,7 +16,10 @@ fi
 if  [ -S /run/redis/redis.sock ]; then
         rm /run/redis/redis.sock
 fi
-redis-server --unixsocket /run/redis/redis.sock --unixsocketperm 700 --timeout 0 --databases 128 --maxclients 512 --daemonize yes --port 6379 --bind 0.0.0.0
+# Does redis need to be bound to 0.0.0.0 or will it work with just local host?
+redis-server --unixsocket /run/redis/redis.sock --unixsocketperm 700 \
+             --timeout 0 --databases $REDISDBS --maxclients 4096 --daemonize yes \
+             --port 6379 --bind 0.0.0.0
 
 echo "Wait for redis socket to be created..."
 while  [ ! -S /run/redis/redis.sock ]; do
@@ -39,31 +35,31 @@ while  [ "${X}" != "PONG" ]; do
 done
 echo "Redis ready."
 
-
-if  [ ! -d /data ]; then
-	echo "Creating Data folder..."
-        mkdir /data
-fi
-
+# This is for a first run with no existing database.
 if  [ ! -d /data/database ]; then
-	echo "Creating Database folder..."
-	mv /var/lib/postgresql/10/main /data/database
-	ln -s /data/database /var/lib/postgresql/10/main
-	chown postgres:postgres -R /var/lib/postgresql/10/main
+	echo "Creating Data and database folder..."
+	mv /var/lib/postgresql/12/main /data/database
+	ln -s /data/database /var/lib/postgresql/12/main
+	chown postgres:postgres -R /var/lib/postgresql/12/main
 	chown postgres:postgres -R /data/database
 fi
 
-if [ ! -L /var/lib/postgresql/10/main ]; then
+# These are  needed for a first run WITH a new container image
+# and an existing database in the mounted volume at /data
+
+if [ ! -L /var/lib/postgresql/12/main ]; then
 	echo "Fixing Database folder..."
-	rm -rf /var/lib/postgresql/10/main
-	ln -s /data/database /var/lib/postgresql/10/main
-	chown postgres:postgres -R /var/lib/postgresql/10/main
+	rm -rf /var/lib/postgresql/12/main
+	ln -s /data/database /var/lib/postgresql/12/main
+	chown postgres:postgres -R /var/lib/postgresql/12/main
 	chown postgres:postgres -R /data/database
 fi
 
 if [ ! -L /usr/local/var/lib  ]; then
 	echo "Fixing local/var/lib ... "
-	if [ ! -d /data/var-lib ]; then  mkdir /data/var-lib; fi
+	if [ ! -d /data/var-lib ]; then
+		mkdir /data/var-lib
+	fi
 	cp -rf /usr/local/var/lib/* /data/var-lib
 	rm -rf /usr/local/var/lib
 	ln -s /data/var-lib /usr/local/var/lib
@@ -76,68 +72,149 @@ if [ ! -L /usr/local/share ]; then
 	ln -s /data/local-share /usr/local/share 
 fi
 
-echo "Starting PostgreSQL..."
-/usr/bin/pg_ctlcluster --skip-systemctl-redirect 10 main start
-
-if [ ! -f "/firstrun" ]; then
-	echo "Running first start configuration..."
-
-	echo "Creating Openvas NVT sync user..."
-	useradd --home-dir /usr/local/share/openvas openvas-sync
-	chown openvas-sync:openvas-sync -R /usr/local/share/openvas
-	chown openvas-sync:openvas-sync -R /usr/local/var/lib/openvas
-	echo "Creating Greenbone Vulnerability system user..."
-	useradd --home-dir /usr/local/share/gvm gvm
-	chown gvm:gvm -R /usr/local/share/gvm
-	if [ ! -d /usr/local/var/lib/gvm/cert-data ]; then mkdir -p /usr/local/var/lib/gvm/cert-data; fi
-	chown gvm:gvm -R /usr/local/var/lib/gvm
-	chmod 770 -R /usr/local/var/lib/gvm
-	chown gvm:gvm -R /usr/local/var/log/gvm
-	chown gvm:gvm -R /usr/local/var/run
-
-	adduser openvas-sync gvm
-	adduser gvm openvas-sync
-	touch /firstrun
+# Postgres config should be tighter.
+# Actually, postgress should be in its own container!
+# maybe redis should too. 
+if [ ! -f "/setup" ]; then
+	echo "Creating postgresql.conf and pg_hba.conf"
+	# Need to look at restricting this. Maybe to localhost ?
+	echo "listen_addresses = '*'" >> /data/database/postgresql.conf
+	echo "port = 5432" >> /data/database/postgresql.conf
+	# This probably tooooo open.
+	echo -e "host\tall\tall\t0.0.0.0/0\ttrust" >> /data/database/pg_hba.conf
+	echo -e "host\tall\tall\t::0/0\ttrust" >> /data/database/pg_hba.conf
+	echo -e "local\tall\tall\ttrust"  >> /data/database/pg_hba.conf
 fi
 
-if [ ! -f "/data/firstrun" ]; then
+echo "Starting PostgreSQL..."
+su -c "/usr/lib/postgresql/12/bin/pg_ctl -D /data/database start" postgres
+
+
+if [ ! -f "/setup" ]; then
+	echo "Running first start configuration..."
+	useradd --home-dir /usr/local/share/gvm gvm
+	chown gvm:gvm -R /usr/local/share/gvm
+	if [ ! -d /usr/local/var/lib/gvm/cert-data ]; then 
+		mkdir -p /usr/local/var/lib/gvm/cert-data; 
+	fi
+
+
+fi
+if [ ! -f "/data/setup" ]; then
 	echo "Creating Greenbone Vulnerability Manager database"
 	su -c "createuser -DRS gvm" postgres
 	su -c "createdb -O gvm gvmd" postgres
 	su -c "psql --dbname=gvmd --command='create role dba with superuser noinherit;'" postgres
 	su -c "psql --dbname=gvmd --command='grant dba to gvm;'" postgres
 	su -c "psql --dbname=gvmd --command='create extension \"uuid-ossp\";'" postgres
-	touch /data/firstrun
+	su -c "psql --dbname=gvmd --command='create extension \"pgcrypto\";'" postgres
+	chown postgres:postgres -R /data/database
+	su -c "/usr/lib/postgresql/12/bin/pg_ctl -D /data/database restart" postgres
+	if [ ! /data/var-lib/gvm/CA/servercert.pem ]; then
+		echo "Generating certs..."
+    	gvm-manage-certs -a
+	fi
+	touch /data/setup
 fi
 
-# Force fix /usr/local/var/lib/run
-#chown root:root /usr/local/var/lib/run
-#chmod 777 /usr/local/var/lib/run 
+# Always make sure these are right.
 
-echo "Fixing sync script options ... "
-sed -i "s/ltvrP/ltrP/" /usr/local/bin/greenbone-nvt-sync
-sed -i "s/ltvrP/ltrP/" /usr/local/sbin/greenbone-scapdata-sync
-sed -i "s/ltvrP/ltrP/" /usr/local/sbin/greenbone-certdata-sync
+chown gvm:gvm -R /usr/local/var/lib/gvm
+chmod 770 -R /usr/local/var/lib/gvm
+chown gvm:gvm -R /usr/local/var/log/gvm
+chown gvm:gvm -R /usr/local/var/run	
 
-echo "Updating NVTs..."
-#su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
-su -c "/usr/local/bin/greenbone-nvt-sync" openvas-sync
-sleep 5
+if [ -d /usr/local/var/lib/gvm/data-objects/gvmd/20.08/report_formats ]; then
+	echo "Creating dir structure for feed sync"
+	for dir in configs port_lists report_formats; do 
+		su -c "mkdir -p /usr/local/var/lib/gvm/data-objects/gvmd/20.08/${dir}" gvm
+	done
+fi
 
-echo "Updating CERT data..."
-su -c "/usr/local/sbin/greenbone-certdata-sync" openvas-sync
-sleep 5
 
-echo "Updating SCAP data..."
-su -c "/usr/local/sbin/greenbone-scapdata-sync" openvas-sync
+su -c "gvmd --migrate" gvm
+
+echo "Updating NVTs and other data"
+echo "This could take a while if you are not using persistent storage for your NVTs"
+echo " or this is the first time pulling to your persistent storage."
+echo " the time will be mostly dependent on your available bandwidth."
+echo " We sleep for 5 seconds between sync command to make sure everything closes"
+echo " and it doesnt' look like we are connecting more than once."
+# Fix perms on var/run for the sync to function
+chmod 777 /usr/local/var/run/
+# And it should be empty. (Thanks felimwhiteley )
+if [ -f /usr/local/var/run/feed-update.lock ]; then
+        # If NVT updater crashes it does not clear this up without intervention
+        echo "Removing feed-update.lock"
+	rm /usr/local/var/run/feed-update.lock
+fi
+
+# This will make the feed syncs a little quieter
+if [ $QUIET == "TRUE" ] || [ $QUIET == "true" ]; then
+	QUIET="true"
+	echo " Fine, ... we'll be quiet, but we warn you if there are errors"
+	echo " syncing the feeds, you'll miss them."
+else
+	QUIET="false"
+fi
+
+if [ $QUIET == "true" ]; then 
+	echo " Pulling NVTs from greenbone" 
+	su -c "/usr/local/bin/greenbone-nvt-sync" gvm 2&> /dev/null
+	echo " Pulling scapdata from greenbone"
+	su -c "/usr/local/sbin/greenbone-scapdata-sync" gvm 2&> /dev/null
+	echo " Pulling cert-data from greenbone"
+	su -c "/usr/local/sbin/greenbone-certdata-sync" gvm 2&> /dev/null
+	echo " Pulling latest GVMD Data from greenbone" 
+	su -c "/usr/local/sbin/greenbone-feed-sync --type GVMD_DATA " gvm 2&> /dev/null
+
+else
+	echo " Pulling NVTs from greenbone" 
+	su -c "/usr/local/bin/greenbone-nvt-sync" gvm
+	echo " Pulling scapdata from greenboon"
+	su -c "/usr/local/sbin/greenbone-scapdata-sync" gvm
+	echo " Pulling cert-data from greenbone"
+	su -c "/usr/local/sbin/greenbone-certdata-sync" gvm
+	echo " Pulling latest GVMD Data from Greenbone" 
+	su -c "/usr/local/sbin/greenbone-feed-sync --type GVMD_DATA " gvm
+
+fi
+
+echo "Starting Greenbone Vulnerability Manager..."
+su -c "gvmd --osp-vt-update=/tmp/ospd.sock" gvm
+
+until su -c "gvmd --get-users" gvm; do
+	echo "Waiting for gvmd"
+	sleep 1
+done
+
+echo "Checking for $USERNAME"
+# Unset this here or the --get-users will kill the script on a normal startup.
+set +e
+su -c "gvmd --get-users | grep -qis $USERNAME " gvm
+if [ $? -ne 0 ]; then
+	echo "$USERNAME does not exist"
+	echo "Creating Greenbone Vulnerability Manager admin user as $USERNAME"
+	su -c "gvmd --role=\"Super Admin\" --create-user=\"$USERNAME\" --password=\"$PASSWORD\"" gvm
+	echo "admin user created"
+	ADMINUUID=$(su -c "gvmd --get-users --verbose | awk '{print \$2}' " gvm)
+	echo "admin user UUID is $ADMINUUID"
+	echo "Granting admin access to defaults"
+	su -c "gvmd --modify-setting 78eceaec-3385-11ea-b237-28d24461215b --value $ADMINUUID" gvm
+fi
+echo "reset "
+set -Eeuo pipefail
+touch /setup
+
+# because ....
+chown -R gvm:gvm /data/var-lib 
+
 
 if [ -f /var/run/ospd.pid ]; then
   rm /var/run/ospd.pid
 fi
 
-if [ -S /tmp/ospd.sock ]; then
-  rm /tmp/ospd.sock
-fi
+
 echo "Starting Postfix for report delivery by email"
 # Configure postfix
 sed -i "s/^relayhost.*$/relayhost = ${RELAYHOST}:${SMTPPORT}/" /etc/postfix/main.cf
@@ -145,51 +222,46 @@ sed -i "s/^relayhost.*$/relayhost = ${RELAYHOST}:${SMTPPORT}/" /etc/postfix/main
 #/usr/lib/postfix/sbin/master -w
 service postfix start
 
-
+if [ -S /tmp/ospd.sock ]; then
+  rm /tmp/ospd.sock
+fi
 echo "Starting Open Scanner Protocol daemon for OpenVAS..."
-ospd-openvas --log-file /usr/local/var/log/gvm/ospd-openvas.log --unix-socket /tmp/ospd.sock --log-level INFO
+ospd-openvas --log-file /usr/local/var/log/gvm/ospd-openvas.log \
+             --unix-socket /tmp/ospd.sock --log-level INFO
 
+# wait for ospd to start by looking for the socket creation.
 while  [ ! -S /tmp/ospd.sock ]; do
 	sleep 1
 done
 
-# This is cludgy and needs a better fix. namely figure out how to hard code alllll of the scoket references in the startup process.
-# It's possible this is a problem with my DB as I only see it when using my DB. 
+# This is cludgy and needs a better fix. namely figure out how to hard code alllll 
+# of the scoket references in the startup process.
+# Update ... I think this is no longer needed.
+# Need to test. Added this back when gvmd --rebuild failed from command line.
+# suspect it would have worked fine if using the --osp-vt-update=/tmp/ospd.sock
 if [ ! -L /var/run/openvassd.sock ]; then
 	echo "Fixing the ospd socket ..."
 	rm -f /var/run/openvassd.sock
 	ln -s /tmp/ospd.sock /var/run/openvassd.sock
+	ln -s /tmp/ospd.sock /var/run/ospd/ospd.sock 
 fi
 
+# I think this could be handled with ospd-openvas options.
+# and likely only needs 660. Or mabye 600 since everything is now owned by gvm.
+# need to test.
 chmod 666 /tmp/ospd.sock
-echo "Migrating database if needed"
-su -c "gvmd -m" gvm
-echo "Starting Greenbone Vulnerability Manager..."
-su -c "gvmd --osp-vt-update=/tmp/ospd.sock" gvm
 
-if [ ! -L /var/run/gvmd.sock ]; then
-	echo "Fixing the gvmd socket ..."
-	rm -f /var/run/gvmd.sock
-	ln -s /usr/local/var/run/gvmd.sock /var/run/gvmd.sock
-fi
 
-until su -c "gvmd --get-users" gvm; do
-	sleep 1
-done
-
-if [ ! -f "/data/created_gvm_user" ]; then
-	echo "Creating Greenbone Vulnerability Manager admin user"
-	su -c "gvmd --create-user=${USERNAME} --password=${PASSWORD}" gvm
-	
-	touch /data/created_gvm_user
-fi
 
 echo "Starting Greenbone Security Assistant..."
 su -c "gsad --verbose --http-only --no-redirect --port=9392" gvm
-
+GVMVER=$(su -c "gvmd --version" gvm ) 
 echo "++++++++++++++++++++++++++++++++++++++++++++++"
-echo "+ Your GVM 11 container is now ready to use! +"
+echo "+ Your GVM/openvas/postgresql container is now ready to use! +"
 echo "++++++++++++++++++++++++++++++++++++++++++++++"
+echo ""
+echo "gvmd --version"
+echo "$GVMVER"
 echo ""
 echo "++++++++++++++++"
 echo "+ Tailing logs +"
