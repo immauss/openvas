160e34efc68264101bf9e70971126efa64b4a244
immauss <scott@immauss.com>
2021-01-03 17:35:20 +0000

    added sleeps to sync and fixed ospd.sock code

--- scripts/start.sh
+++ scripts/start.sh
@@ -161,20 +161,26 @@ fi
 if [ $QUIET == "true" ]; then 
 	echo " Pulling NVTs from greenbone" 
 	su -c "/usr/local/bin/greenbone-nvt-sync" gvm 2&> /dev/null
+	sleep 2
 	echo " Pulling scapdata from greenbone"
 	su -c "/usr/local/sbin/greenbone-scapdata-sync" gvm 2&> /dev/null
+	sleep 2
 	echo " Pulling cert-data from greenbone"
 	su -c "/usr/local/sbin/greenbone-certdata-sync" gvm 2&> /dev/null
+	sleep 2
 	echo " Pulling latest GVMD Data from greenbone" 
 	su -c "/usr/local/sbin/greenbone-feed-sync --type GVMD_DATA " gvm 2&> /dev/null
 
 else
 	echo " Pulling NVTs from greenbone" 
 	su -c "/usr/local/bin/greenbone-nvt-sync" gvm
+	sleep 2
 	echo " Pulling scapdata from greenboon"
 	su -c "/usr/local/sbin/greenbone-scapdata-sync" gvm
+	sleep 2
 	echo " Pulling cert-data from greenbone"
 	su -c "/usr/local/sbin/greenbone-certdata-sync" gvm
+	sleep 2
 	echo " Pulling latest GVMD Data from Greenbone" 
 	su -c "/usr/local/sbin/greenbone-feed-sync --type GVMD_DATA " gvm
 
@@ -239,10 +245,11 @@ done
 # Update ... I think this is no longer needed.
 # Need to test. Added this back when gvmd --rebuild failed from command line.
 # suspect it would have worked fine if using the --osp-vt-update=/tmp/ospd.sock
-if [ ! -L /var/run/openvassd.sock ]; then
+if [ ! -L /var/run/ospd/ospd.sock ]; then
 	echo "Fixing the ospd socket ..."
 	#rm -f /var/run/openvassd.sock
 	#ln -s /tmp/ospd.sock /var/run/openvassd.sock
+	rm -f /var/run/ospd/ospd.sock
 	ln -s /tmp/ospd.sock /var/run/ospd/ospd.sock 
 fi
 
