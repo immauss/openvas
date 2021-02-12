149c3d2b62e5e36ffa6e34570981977e34488d81
immauss <scott@immauss.com>
2021-02-12 13:03:22 +0000

    fixed url for git clone of ospd-openvas

--- build.d/08-ospd-openvas.sh
+++ build.d/08-ospd-openvas.sh
@@ -2,8 +2,10 @@
 set -Eeuo pipefail
 echo "Building ospd-openvas"   
 cd /build
-git clone https://github.com/greenbone/ospd.git
+git clone https://github.com/greenbone/ospd-openvas.git
+#git clone --single-branch --branch ospd-openvas-20.08 https://github.com/greenbone/ospd.git
 cd /build/*/
 python3 setup.py install
 cd /build
 rm -rf *
+
