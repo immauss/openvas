75b2856a952f708c8f2a3681274922d608304cac
immauss <scott@immauss.com>
2021-02-17 10:34:35 +0000

    updated to use a temp container and the latest image

--- refresh.sh
+++ refresh.sh
@@ -4,46 +4,41 @@
 # This should run from a cron with a long enough sleep to make sure
 # the gvmd has updated the DB before creating the archive and pushing
 # to github.
-BUILDDIR="/home/scott/Projects/openvas"
+# Temp working directory ... needs enough space to pull the entire feed and then compress it. ~2G
 TWD="/var/lib/openvas"
-WD=$(pwd)
-CNAME="openvas-base-data" # name of the container running for updates.
 STIME="15m" # time between resync and archiving.
 
-echo "Restart the container to force an update"
-docker restart $CNAME
-
-
+echo "Starting container for an update"
+docker run -d --rm --name updater immauss/openvas
 echo "Sleeping for $STIME to make sure the feeds are updated in the db"
 sleep $STIME
 
 cd $TWD
 echo "First copy the feeds from the container"
-docker cp ${CNAME}:/data/var-lib .
+docker cp updater:/data/var-lib .
 echo "Now dump the db from postgres"
-#docker exec -i $CNAME su -c "/usr/lib/postgresql/12/bin/pg_dump gvmd" postgres > ./base.sql 
-docker exec -i $CNAME su -c "/usr/lib/postgresql/12/bin/pg_dumpall" postgres > ./base.sql 
+docker exec -i updater su -c "/usr/lib/postgresql/12/bin/pg_dumpall" postgres > ./base.sql 
+
+echo "Stopping update container"
+docker stop updater
+
 
 echo "Compress and archive the data"
 tar cJf var-lib.tar.xz var-lib
 xz base.sql
-
-echo "Do some cleanup"
-mv var-lib.tar.xz $WD
-mv base.sql.xz $WD
-
-rm -rf var-lib
-cd $WD
-echo "All done!"
-ls -l *.xz
-DATE=$(date)
-#git commit -a -m "Data update for $DATE"
-#git push
-
 scp *.xz push@www.immauss.com:/var/www/html/openvas/
 
-cd $BUILDDIR
-touch Dockerfile
-echo $DATE > .base-ts
+git clone git+ssh://git@github.com/immauss/openvas.git
+cd openvas
+
+date > update.ts
+
+echo "Cleaning up"
+cd ..
+rm -rf openvas var-lib *.xz
+
+echo "All done"
+
+
 git commit -a -m "Data update for $Date"
 git push
