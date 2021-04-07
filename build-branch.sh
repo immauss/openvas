a28f19f1634a7f901284d3552cf82b4ebc407e39
immauss <scott@immauss.com>
2021-04-07 09:56:57 +0000

    Added test container start and log tail to build-branch.sh

--- build-branch.sh
+++ build-branch.sh
@@ -1,1 +1,5 @@
-docker build -t immauss/openvas:$(git branch | awk /\*/'{print $2}') .
+BRANCH=$(git branch | awk /\*/'{print $2}')
+docker build -t immauss/openvas:$BRANCH .
+echo " Build complete. Starting test instance."
+docker run -d --name "${BRANCH}-test" immauss/openvas:${BRANCH}
+docker logs -f "${BRANCH}-test"
