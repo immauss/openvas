96fa3b9b6ccbad084414dfb5430ad0fac7bec43e
immauss <scott@immauss.com>
2021-04-26 20:38:10 +0000

    Updates for 21.04 new pre=reqs and trying to fix missing tables in db.

--- build-branch.sh
+++ build-branch.sh
@@ -1,5 +1,8 @@
 BRANCH=$(git branch | awk /\*/'{print $2}')
 docker build -t immauss/openvas:$BRANCH .
+if [ $? -ne 0 ]; then
+	echo "Build failed. :("
+fi
 echo " Build complete. Starting test instance."
 docker rm -f ${BRANCH}-test
 docker run -d --name "${BRANCH}-test" immauss/openvas:${BRANCH}
