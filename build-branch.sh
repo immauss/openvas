c6b6c8f3ae10fcb438af4fca245905ef22eafefa
immauss <scott@immauss.com>
2021-06-01 04:47:53 +0000

    fixes for build processes

--- build-branch.sh
+++ build-branch.sh
@@ -2,5 +2,5 @@ BRANCH=$(git branch | awk /\*/'{print $2}')
 docker build -t immauss/openvas:$BRANCH . 2>&1 | tee build.log
 echo " Build complete. Starting test instance."
 docker rm -f ${BRANCH}-test
-docker run -d -p 9392:9000 --name "${BRANCH}-test" immauss/openvas:${BRANCH}
+docker run -d -p 9000:9392 --name "${BRANCH}-test" immauss/openvas:${BRANCH}
 docker logs -f "${BRANCH}-test"
