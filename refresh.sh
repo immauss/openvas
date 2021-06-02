13e68bff1e523820a6ac7a8ccc59bba58a7f7dcc
immauss <scott@immauss.com>
2021-06-02 10:43:15 +0000

    Added storage check before doing anything Change TAG to latest now that moving to next branch is complete.

--- refresh.sh
+++ refresh.sh
@@ -5,15 +5,26 @@
 # the gvmd has updated the DB before creating the archive and pushing
 # to github. It's probably not going to be useful to anyone but me
 # but the output will benefit all. 
+
+# First, make sure there's enough storage avaialbe before doing anything.
+SPACE=$(df -h /var/lib/docker | awk /G/'{print $4}' | sed "s/G//")
+if [ -n $SPACE ]; then
+	echo "Check avaialbe storage"
+	exit
+elif [ $SPACE -le 4 ]; then
+	echo "only ${SPACE}G of space on /var/lib/docker ... bailing out."
+	exit
+fi
+
 # Tag to work with. Normally latest but might be using new tag during upgrades.
-TAG="21.04"
+TAG="latest"
 # Temp working directory ... needs enough space to pull the entire feed and then compress it. ~2G
 TWD="/var/lib/openvas"
-STIME="45m" # time between resync and archiving.
+STIME="1h" # time between resync and archiving.
 # Force a pull of the latest image.
 docker pull immauss/openvas:$TAG
 echo "Starting container for an update"
-docker run -d --rm --name updater immauss/openvas:$TAG
+docker run -d --name updater immauss/openvas:$TAG
 date
 echo "Sleeping for $STIME to make sure the feeds are updated in the db"
 sleep $STIME
@@ -26,6 +37,10 @@ docker exec -i updater su -c "/usr/lib/postgresql/12/bin/pg_dumpall" postgres > 
 
 echo "Stopping update container"
 docker stop updater
+echo "Dumping container logs to /var/log/refresh.log"
+date >> /var/log/refresh.log
+docker logs updater >> /var/log/refresh.log
+docker rm updater
 
 echo "Compress and archive the data"
 tar cJf var-lib.tar.xz --exclude=var-lib/gvm/gvmd/gnupg var-lib
