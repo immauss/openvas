623ef30ad63279fc76d5db74287c24ef6be66166
Scott <scott@immauss.com>
2021-07-19 15:17:23 +0000

    Working Buster for AMD64 & Arm64

--- base-rebuild.sh
+++ base-rebuild.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -Eeuo pipefail
 cd /home/scott/Projects/openvas/ovasbase
-docker build -f Dockerfile.buster -t immauss/ovasbase:buster .
+docker buildx build --push --platform linux/amd64,linux/arm64 -f Dockerfile.buster -t immauss/ovasbase:buster .
 
 cd ..
 
-docker build -f Dockerfile.buster -t immauss/openvas:buster .
+docker buildx build --push --no-cache --platform linux/amd64,linux/arm64 -f Dockerfile.buster -t immauss/openvas:buster .
 
 docker rm -f buster
+docker pull immauss/openvas:buster
 docker run -d --name buster immauss/openvas:buster 
 docker logs -f buster
 
