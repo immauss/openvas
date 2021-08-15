c4300f518081c8ee18f73aa91120e9d62054db3e
Scott <scott@immauss.com>
2021-08-15 07:38:30 +0000

    standardizing on buster

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -4,6 +4,7 @@ set -Eeuo pipefail
 export DEBIAN_FRONTEND=noninteractive
 export LANG=C.UTF-8
 
+apt update
 PACKAGES=$(cat package-list-building)
 apt-get install -yq --no-install-recommends $PACKAGES
 /usr/sbin/update-ca-certificates --fresh
