9181618f2694721407dc7d373305888505e4a95d
Scott <scott@immauss.com>
2021-08-25 16:56:26 +0000

    added base rebuild script

--- base-rebuild.sh
+++ base-rebuild.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+tag=armvt
 set -Eeuo pipefail
 cd /home/scott/Projects/openvas/ovasbase
-docker buildx build --push --platform linux/arm/v7 -t immauss/ovasbase:armv7 .
+docker buildx build --push --no-cache --platform  linux/amd64,linux/arm64 -f Dockerfile -t immauss/ovasbase:$tag .
 
 cd ..
 
-docker buildx build --push --no-cache --platform linux/arm/v7 -t immauss/openvas:armv7 .
+docker buildx build --push --no-cache --platform linux/amd64,linux/arm64 -f Dockerfile -t immauss/openvas:$tag .
+
+docker rm -f $tag
+docker pull immauss/openvas:$tag
+docker run -d --name $tag immauss/openvas:$tag 
+docker logs -f $tag
 
