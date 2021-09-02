6458e152a9e0154161d2b86dba128183fbc26355
Scott <scott@immauss.com>
2021-09-02 07:59:03 +0000

    modified psutil install

--- build.d/build-prereqs.sh
+++ build.d/build-prereqs.sh
@@ -4,7 +4,10 @@ set -Eeuo pipefail
 . build.rc
 apt update
 echo "install required packages"
+df -h
+ls -l /tmp
 PACKAGES=$(cat package-list-buster-build)
 apt-get install -yq --no-install-recommends $PACKAGES
+
 
 python3 -m pip install psutil
