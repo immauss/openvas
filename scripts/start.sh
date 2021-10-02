b1f2920d60c720d02d0d2be4b0f88ad7774f66f0
Scott <scott@immauss.com>
2021-10-02 14:14:30 +0000

    Mods to align refresh.sh, tighter tls settings on allows redirects from http -> https

--- scripts/start.sh
+++ scripts/start.sh
@@ -417,8 +417,7 @@ echo "Starting Greenbone Security Assistant..."
 #su -c "gsad --verbose --http-only --no-redirect --port=9392" gvm
 if [ $HTTPS == "true" ]; then
 	su -c "gsad --mlisten 127.0.0.1 -m 9390 --verbose --timeout=$GSATIMEOUT \
-	            --gnutls-priorities=SECURE128:-AES-128-CBC:-CAMELLIA-128-CBC:-VERS-SSL3.0:-VERS-TLS1.0 \
-		    --no-redirect \
+		    --gnutls-priorities=SECURE128:+SECURE192:-VERS-TLS-ALL:+VERS-TLS1.2 \
 		    --port=9392" gvm
 else
 	su -c "gsad --mlisten 127.0.0.1 -m 9390 --verbose --timeout=$GSATIMEOUT --http-only --no-redirect --port=9392" gvm
