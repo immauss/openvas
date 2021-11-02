ef3b93d42532f47dbba3a78bcc6bb93416534942
Scott <scott@immauss.com>
2021-11-02 11:26:06 +0000

    Removed a few more useless bits and update the bin/Readme.md with descriptions of scripts in bin/

--- bin/progressive-build.sh
+++
@@ -1,3 +0,0 @@
-docker buildx build -t immauss/ovasbase:latest  --platform linux/amd64 .
-docker buildx build -t immauss/ovasbase:latest  --platform linux/arm64 .
-docker buildx build -t immauss/ovasbase:latest  --platform linux/amd64,linux/arm64 --push .
