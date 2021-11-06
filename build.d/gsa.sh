fc5de496f2a28275f9d7f2456d5d2ec610bcf543
Scott <scott@immauss.com>
2021-11-06 21:40:07 +0000

    merge build.d from master

--- build.d/gsa.sh
+++ build.d/gsa.sh
@@ -6,10 +6,10 @@ echo "Building gsa"
 cd /build
 GSA_VERSION=$(echo $gsa| sed "s/^v\(.*$\)/\1/")
 curl -f -L https://github.com/greenbone/gsa/archive/refs/tags/v$GSA_VERSION.tar.gz -o $gsa.tar.gz
-curl -f -L https://github.com/greenbone/gsa/releases/download/v$GSA_VERSION/gsa-node-modules-$GSA_VERSION.tar.gz -o gsa-node-modules-$GSA_VERSION.tar.gz
+#curl -f -L https://github.com/greenbone/gsa/releases/download/v$GSA_VERSION/gsa-node-modules-$GSA_VERSION.tar.gz -o gsa-node-modules-$GSA_VERSION.tar.gz
 mkdir -p gsa-$GSA_VERSION/gsa
 tar -xf $gsa.tar.gz
-tar -C gsa-$GSA_VERSION/gsa -xf gsa-node-modules-$GSA_VERSION.tar.gz
+#tar -C gsa-$GSA_VERSION/gsa -xf gsa-node-modules-$GSA_VERSION.tar.gz
 
 cd /build/*/
 mkdir build
