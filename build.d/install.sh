2abf7409b4ba03114494d3f022acc3d6391da114
Scott <scott@immauss.com>
2021-11-23 10:29:37 +0000

    install.sh for manual builds in ovasbase container start-test.sh to spin up new image with previous version /data

---
+++ build.d/install.sh
@@ -0,0 +1,10 @@
+bash /mnt/build.d/build-prereqs.sh && \
+	bash /mnt/build.d/gvm-libs.sh && \
+	bash /mnt/build.d/openvas-smb.sh && \
+	bash /mnt/build.d/gvmd.sh && \
+	bash /mnt/build.d/openvas-scanner.sh && \
+	bash /mnt/build.d/gsa.sh && \
+	bash /mnt/build.d/ospd.sh && \
+	bash /mnt/build.d/ospd-openvas.sh && \
+	bash /mnt/build.d/gvm-tool.sh && \
+	bash /mnt/build.d/links.sh
