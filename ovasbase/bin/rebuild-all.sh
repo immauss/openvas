c621ed76e6bfbe0a6fb97fd2e611d326db408923
Scott <scott@immauss.com>
2022-04-28 11:32:25 +0000

    updating MC branch with ovasbase from master

---
+++ ovasbase/bin/rebuild-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+tag=$1
+if [ -z $tag ] ; then
+        tag="latest"
+else
+        tag="$tag"
+fi
+docker buildx build -t immauss/ovasbase:$tag --platform linux/arm64,linux/amd64 --push  .
