273711fac692c2c49d6acd39b9e4ae394d5f7e8f
Scott <scott@immauss.com>
2022-05-13 04:34:13 +0000

    Working db-upgrade on sinlge and multi-container setup

--- scripts/db-upgrade.sh
+++ scripts/db-upgrade.sh
@@ -10,8 +10,17 @@
 # Delete old DB
 # mv new/upgraded DB to /data/database
 # Hope for the best ...
+
+# First, let's make sure we have some logs to go on. 
+su -c "/usr/lib/postgresql/13/bin/postgres -D /data/database >> /var/log/postgresql/postgresql-gvmd.log 2>&1 &" postgres
+# wait for it to fail.
+sleep 5
+echo " Log status ...."
+ls -l /var/log/postgresql
+tail -50 /var/log/postgresql/postgresql-gvmd.log 
+echo " Log status done. " 
 echo "Looking for indication of database mismatch."
-STATUS=$(tail -25 /var/log/postgresql/postgresql-gvmd.log | awk /DETAIL:.*Server.is.version/'{ print $(NF - 4) $NF}')
+STATUS=$(tail -25 /var/log/postgresql/postgresql-gvmd.log | awk /initialized.by.PostgreSQL.version/'{ print $14 $22}')
 if [ -z $STATUS ]; then
 	echo "There does not appear to be a difference in databases versions.... $STATUS"
 	echo "exiting now"
@@ -21,7 +30,7 @@ else
 fi
 
 OLD=$(echo $STATUS | sed -e "s/,/ /;s/\./ /" | awk '{print $1}')
-NEW=$(echo $STATUS | sed -e "s/,/ /;s/\./ /" | awk '{print $2}')
+NEW=$(echo $STATUS | sed -e "s/,/ /;s/\..*/ /" | awk '{print $2}')
 
 echo "Installing postgresql-$OLD"
 apt update
@@ -45,8 +54,16 @@ cd /data/db-upgrade
 su -c  "/usr/lib/postgresql/$NEW/bin/pg_upgrade " postgres
 if [ $? -eq 0 ]; then
 	echo "Looks like a success!!"
-	exit $?
+	echo "mv $PGDATAOLD to $PGDATAOLD.$OLD"
+	mv $PGDATAOLD $PGDATAOLD.$OLD
+	echo "mv $PGDATANEW to $PGDATAOLD"
+	mv $PGDATANEW $PGDATAOLD
+	
+	echo " Upgrade complete !"
+	echo " Returning to startup"
+	exit 0
 else
 	echo " Uh Oh! $?"
 	exit $?
 fi
+
