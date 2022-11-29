a48f3fceed2425bfa36c8f6c32deb91b37df2056
Scott <scott@immauss.com>
2022-11-29 23:02:23 +0000

    updates to healthcheck.sh to properly report failure

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -22,27 +22,30 @@ case  $FUNC in
 		nmap -p 5432 localhost| grep -qs "5432.*open" || exit 1
 	;;
 	single)
+		FAIL=0
 		# openvas
 		UUID=$( su -c "gvmd --get-scanners" gvm | awk /OpenVAS/'{print  $1}' )
-		su -c "gvmd --verify-scanner=$UUID" gvm | grep OpenVAS || FAIL=1
-		echo "UNHEATHLY CONTAINER: openvas healthcheck failed."
+		su -c "gvmd --verify-scanner=$UUID" gvm | grep OpenVAS || FAIL=1 
+			if [ $FAIL -eq 1]; then SERVCE="openas"; fi
 		# gvmd
-		nmap -p 9390 localhost| grep -qs "9390.*open" || FAIL=1
-		echo "UNHEALTHY CONTAINER: gvmd healthcheck failed."
+		nmap -p 9390 localhost| grep -qs "9390.*open" || FAIL=2 
+			if [ $FAIL -eq 2 ]; then SERVICE="$SERVICE gvmd"; fi
 		# gsad
-		curl -f http://localhost:9392/ || curl -kf https://localhost:9392/ || FAIL=1
-		echo "UNHEALTHY CONTAINER: gsad healthcheck failed."
+		curl -f http://localhost:9392/ || curl -kf https://localhost:9392/ || FAIL=3 
+			if [ $FAIL -eq 3 ]; then SERVICE="$SERVICE gsad"; fi
 		# redis
-		redis-cli -s /run/redis/redis.sock ping || FAIL=1
-		echo "UNHEALTHY CONTAINER: redis healthcheck failed."
+		redis-cli -s /run/redis/redis.sock ping || FAIL=4 
+			if [ $FAIL -eq 4 ]; then SERVICE="$SERVICE redis"; fi
 		# postgresql
-		nmap -p 5432 localhost| grep -qs "5432.*open" || FAIL=1
-		echo "UNHEALTHY CONTAINER: postgresql healthcheck failed."
-		if [ $FAIL -eq 1 ]; then
-			echo " HEALTHECHECK FAILED ! ";
+		nmap -p 5432 localhost| grep -qs "5432.*open" || FAIL=5 
+			if [ $FAIL -eq 5 ]; then SERVICE="$SERVICE postgresql"; fi
+		if [ $FAIL -ne 0 ]; then
+			echo " HEALTHECHECK FAILED ! " >> /usr/local/var/log/gvm/healthchecks.log
+			echo " These services failed"  >> /usr/local/var/log/gvm/healthchecks.log
+			echo " $SERVICE" >> /usr/local/var/log/gvm/healthchecks.log
 			exit 1
 		else
-			echo " Healthchecks completed with no issues."
+			echo " Healthchecks completed with no issues." >> /usr/local/var/log/gvm/healthchecks.log
 
 		fi	
 		
