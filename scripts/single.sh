68dcd479576f4598d3bd62bd0caa546c8a28b947
Scott <scott@immauss.com>
2023-05-09 08:21:48 +0000

    added gpg home for openvas for signature validation

--- scripts/single.sh
+++ scripts/single.sh
@@ -377,21 +377,21 @@ allow_anonymous true" >> /etc/mosquitto.conf
 
 echo "Starting Open Scanner Protocol daemon for OpenVAS..."
 # Prep the gpg keys
-#export OPENVAS_GNUPG_HOME=/etc/openvas/gnupg
-#export GNUPGHOME=/tmp/openvas-gnupg
-#if ! [ -f tmp/GBCommunitySigningKey.asc ]; then
-	#echo " Get the Greenbone public Key"
-	#curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /tmp/GBCommunitySigningKey.asc
-	#echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" > /tmp/ownertrust.txt
-	#echo "Setup environment"
-	#mkdir -m 0600 -p $GNUPGHOME $OPENVAS_GNUPG_HOME
-	#echo "Import the key "
-	#gpg --import /tmp/GBCommunitySigningKey.asc
-	#gpg --import-ownertrust < /tmp/ownertrust.txt
-	#echo "Setup key for openvas .."
-	#cp -r /tmp/openvas-gnupg/* $OPENVAS_GNUPG_HOME/
-	#chown -R gvm:gvm $OPENVAS_GNUPG_HOME
-#fi
+export OPENVAS_GNUPG_HOME=/etc/openvas/gnupg
+export GNUPGHOME=/etc/openvas-gnupg
+if ! [ -f tmp/GBCommunitySigningKey.asc ]; then
+	echo " Get the Greenbone public Key"
+	#curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /etc/GBCommunitySigningKey.asc
+	#echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" > /etc/ownertrust.txt
+	echo "Setup environment"
+	mkdir -m 0600 -p $GNUPGHOME $OPENVAS_GNUPG_HOME
+	echo "Import the key "
+	gpg --import /tmp/GBCommunitySigningKey.asc
+	gpg --import-ownertrust < /tmp/ownertrust.txt
+	echo "Setup key for openvas .."
+	cp -r /etc/openvas-gnupg/* $OPENVAS_GNUPG_HOME/
+	chown -R gvm:gvm $OPENVAS_GNUPG_HOME
+fi
 
 /usr/local/bin/ospd-openvas --unix-socket /var/run/ospd/ospd-openvas.sock \
 	--pid-file /run/ospd/ospd-openvas.pid \
