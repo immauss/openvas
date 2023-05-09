077c2a3fe8152b59265ca8d64b6843f1f84590cd
Scott <scott@immauss.com>
2023-05-09 08:20:46 +0000

    added openvas gpg home var for singature validations.

--- build.d/gpg-keys.sh
+++ build.d/gpg-keys.sh
@@ -4,13 +4,13 @@ export OPENVAS_GNUPG_HOME=/etc/openvas/gnupg
 export GNUPGHOME=/tmp/openvas-gnupg
 if ! [ -f tmp/GBCommunitySigningKey.asc ]; then
         echo " Get the Greenbone public Key"
-        curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /tmp/GBCommunitySigningKey.asc
-        echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" > /tmp/ownertrust.txt
+        curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /etc/GBCommunitySigningKey.asc
+        echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" > /etc/ownertrust.txt
         echo "Setup environment"
         mkdir -m 0600 -p $GNUPGHOME $OPENVAS_GNUPG_HOME
         echo "Import the key "
-        gpg --import /tmp/GBCommunitySigningKey.asc
-        gpg --import-ownertrust < /tmp/ownertrust.txt
+        gpg --import /etc/GBCommunitySigningKey.asc
+        gpg --import-ownertrust < /etc/ownertrust.txt
         echo "Setup key for openvas .."
         cp -r /tmp/openvas-gnupg/* $OPENVAS_GNUPG_HOME/
         #chown -R gvm:gvm $OPENVAS_GNUPG_HOME
