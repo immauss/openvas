76c6b3db9ed0e01e6dc1acbca6ca9a3973455692
Scott <scott@immauss.com>
2023-08-27 07:03:42 +0000

    Resolve issue with NEWDB & refresh.  Updates to Readme.md and move to Forum

--- bin/base-rebuild.sh
+++ bin/base-rebuild.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+STARTTIME=$(date +%s)
 NOBASE="false"
+RUNAFTER="1"
+ARM="false"
+ARMSTART=true
+PRUNESTART=true
+BASESTART=true
+TimeMath() {
+    local total_seconds="$1"
+    local hours=$((total_seconds / 3600))
+    local minutes=$(( (total_seconds % 3600) / 60 ))
+    local seconds=$((total_seconds % 60))
+
+    printf "%02d:%02d:%02d\n" "$hours" "$minutes" "$seconds"
+}
 while ! [ -z "$1" ]; do
   case $1 in
 	-t)
@@ -14,7 +28,9 @@ while ! [ -z "$1" ]; do
 	;;
 	-p)
 	echo " Flushing build kit cache"
+	PRUNESTART=$(date +%s)
 	docker buildx prune -af 
+	PRUNEFIN=$(date +%s)
 	shift
 	;;
 	-N)
@@ -38,20 +54,60 @@ if [ "$tag" == "beta" ]; then
 	arch="linux/amd64"
 elif [ -z $arch ]; then
 	arch="linux/amd64,linux/arm64,linux/arm/v7"
+	ARM="true"
 fi
 
 echo "Building with $tag and $arch"
 set -Eeuo pipefail
 if  [ "$NOBASE" == "false" ]; then
 	cd /home/scott/Projects/openvas/ovasbase
+	BASESTART=$(date +%s)
 	docker buildx build --push  --platform  $arch -f Dockerfile -t immauss/ovasbase  .
+	BASEFIN=$(date +%s)
 	cd ..
 fi
 cd /home/scott/Projects/openvas
+# Because the arm64 build seems to always fail when building a the same time as the other archs ....
+# We'll build it first to have it cached for the final build. But we only need the slim
+#
+if [ "$ARM" == "true" ]; then
+	ARM64START=$(date +%s)
+	docker buildx build --build-arg TAG=${tag} --push --platform linux/arm64 -f Dockerfile --target slim -t immauss/openvas:${tag}-slim .
+	ARM64FIN=$(date +%s)
+fi
+# Now build everything together. At this point, this will normally only be the arm7 build as the amd64 was likely built and cached as beta.
+SLIMSTART=$(date +%s)
 docker buildx build --build-arg TAG=${tag} --push --platform $arch -f Dockerfile --target slim -t immauss/openvas:${tag}-slim .
+SLIMFIN=$(date +%s)
+FINALSTART=$(date +%s)
 docker buildx build --build-arg TAG=${tag} --push --platform $arch -f Dockerfile --target final -t immauss/openvas:${tag} .
-docker rm -f $tag
+FINALFIN=$(date +%s)
+
+
+echo "Statistics:"
+# First the dependent times.
+if ! [ $PRUNESTART ]; then
+	PRUNE=$(expr $PRUNEFIN - $PRUNESTART)
+	echo "Build Kit Cache flush: $(Timemath $PRUNE)"
+fi
+if ! [ $BASESTART ]; then 
+	BASE=$(expr $BASEFIN - $BASESTART )
+	echo "ovasbase build time: $(TimeMath $BASE)"
+fi
+if ! [ $ARMSTART ]; then
+	ARM=$(expr $ARMFIN -$ARMSTART )
+	echo "ARM64 Image build time: $(TimeMath $ARM)"
+fi
+# These always run
+SLIM=$(expr $SLIMFIN - $SLIMSTART )
+FINAL=$(expr  $FINALFIN - $FINALSTART )
+FULL=$(expr $FINALFIN - $STARTTIME )
+echo "Slim Image build time: $(TimeMath $SLIM)"
+echo "Final Image build time: $(TimeMath $FINAL)"
+echo "Total run time: $(TimeMath $FULL)"
+
 if [ $RUNAFTER -eq 1 ]; then
+	docker rm -f $tag
 	docker pull immauss/openvas:$tag
 	docker run -d --name $tag -e SKIPSYNC=true -p 8080:9392 immauss/openvas:$tag 
 	docker logs -f $tag
