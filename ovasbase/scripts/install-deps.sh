1df6f5f74d04e5cac6d0ded833536a6b0ee865fb
Scott <scott@immauss.com>
2023-10-02 13:30:41 +0000

    removed yarn install from ovasbase

--- ovasbase/scripts/install-deps.sh
+++ ovasbase/scripts/install-deps.sh
@@ -34,7 +34,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg |  apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" |  tee /etc/apt/sources.list.d/yarn.list
 
  apt update
- apt install -y nodejs yarn
+ apt install -y nodejs
 
 #Clean up after apt
 rm -rf /var/lib/apt/lists/*
