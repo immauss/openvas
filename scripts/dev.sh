6dd0e301c6a442eb3c2c52c9e2ea27fbe42dbb15
immauss <scott@immauss.com>
2024-12-14 15:58:39 +0100

    udpated dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,11 +5,13 @@ cd /gsa
 if [ "$REFRESH" == "true" ]; then
     echo "Copying source from gsa.latest to gsa.dev"
     cp -rp gsa.latest/* gsa.dev/
+    cd /gsa/gsa.dev
+    npm install vite
 else
     echo "Using existing code in gsa.dev"
 fi
 
-cd gsa.dev/
+cd /gsa/gsa.dev/
 npm run start  --host --fix
 /bin/sh
 #gsad --http-cors="http://127.0.0.1:8080"
