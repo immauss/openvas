a15b0dbf2ae4156e685891c19229857a211a207e
immauss <scott@immauss.com>
2025-08-22 22:39:23 +0200

    reworked the gsa-dev container and scripts.

--- build.d/dev-setup.sh
+++ build.d/dev-setup.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 NODE_VERSION=node_20.x
 NODE_KEYRING=/usr/share/keyrings/nodesource.gpg
-DISTRIBUTION=bookworm
+DISTRIBUTION=nodistro
 . /build.rc
 echo "Downloading latest gsa code"  
 GSA_VERSION=$(echo $gsa| sed "s/^v\(.*$\)/\1/")
-mkdir -p /gsa
-cd /gsa
-curl -f -L https://github.com/greenbone/gsa/archive/refs/tags/v$GSA_VERSION.tar.gz -o $gsa.tar.gz
-tar -xf $gsa.tar.gz
-rm $gsa.tar.gz
-cd *
-SRCDIR=$(pwd)
-echo "$SRCDIR" > /sourcedir
-echo "Source directory is $SRCDIR"
-mv $SRCDIR /gsa/gsa.latest
-cd /gsa/gsa.latest
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee "$NODE_KEYRING" >/dev/null && \
-    echo "deb [signed-by=$NODE_KEYRING] https://deb.nodesource.com/$NODE_VERSION $DISTRIBUTION main" | tee /etc/apt/sources.list.d/nodesource.list
+# Remove any old keys
+rm -f /etc/apt/trusted.gpg.d/nodesource.gpg
+rm -f /usr/share/keyrings/nodesource.gpg
+
+# Download and add the NodeSource key
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key \
+  | gpg --dearmor -o $NODE_KEYRING
+
+echo "deb [signed-by=$NODE_KEYRING] https://deb.nodesource.com/$NODE_VERSION $DISTRIBUTION main" >  /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=$NODE_KEYRING] https://deb.nodesource.com/$NODE_VERSION $DISTRIBUTION main" >> /etc/apt/sources.list.d/nodesource.list
+#gpg --no-default-keyring --keyring "$NODE_KEYRING" --list-keys
 apt update && apt install nodejs -y 
 
-npm install vite
-npm audit fix 
-echo "Updating npm"
-npm install -g npm@10.1.0
-echo "Updating npm browserlist"
-npm install
+#npm audit fix 
+#echo "Updating npm"
+#npm install -g npm@10.1.0
+#echo "Updating npm browserlist"
+#npm install
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -8,13 +8,16 @@ if [ "$REFRESH" == "true" ]; then
     cp -rvp gsa.latest/* gsa.dev/
     cd /gsa/gsa.dev
     ls -l
-    npm install vite
+    # npm install vite
 else
     echo "Using existing code in gsa.dev"
 fi
 
 cd /gsa/gsa.dev/
-npm run start  --host --fix
+echo "Now install vite"
+cd /gsa/gsa.dev
+npm install vite
+npm run start --host --fix
 /bin/sh
 #gsad --http-cors="http://127.0.0.1:8080"
 
